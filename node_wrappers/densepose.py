c030efb0387379930d9db1b5ebe0c6476d76fce3
Ziad Abdeltawab <ziad.tawab2000@gmail.com>
2024-02-16 14:41:36 +0200

    Update densepose.py

--- node_wrappers/densepose.py
+++ node_wrappers/densepose.py
@@ -6,20 +6,31 @@ class DensePose_Preprocessor:
     def INPUT_TYPES(s):
         return create_node_input_types(
             model=(["densepose_r50_fpn_dl.torchscript", "densepose_r101_fpn_dl.torchscript"], {"default": "densepose_r50_fpn_dl.torchscript"}),
-            cmap=(["Viridis (MagicAnimate)", "Parula (CivitAI)"], {"default": "Viridis (MagicAnimate)"})
+            cmap=(["Viridis (MagicAnimate)", "Parula (CivitAI)"], {"default": "Viridis (MagicAnimate)"}),
+            cache_model = (["enable", "disable"], {"default": "enable"}),
+
         )
 
     RETURN_TYPES = ("IMAGE",)
     FUNCTION = "execute"
+    model = None
 
     CATEGORY = "ControlNet Preprocessors/Faces and Poses Estimators"
 
-    def execute(self, image, model, cmap, resolution=512):
+    def execute(self, image, model, cmap, cache_model, resolution=512):
+        cache_model = cache_model == "enable"
+
         from controlnet_aux.densepose import DenseposeDetector
-        model = DenseposeDetector \
-                    .from_pretrained(filename=model) \
-                    .to(model_management.get_torch_device())
-        return (common_annotator_call(model, image, cmap="viridis" if "Viridis" in cmap else "parula", resolution=resolution), )
+        if self.model is None:
+            self.model = DenseposeDetector \
+                        .from_pretrained(filename=model) \
+                        .to(model_management.get_torch_device())
+        output = common_annotator_call(self.model, image, cmap="viridis" if "Viridis" in cmap else "parula", resolution=resolution) 
+        
+        if not cache_model:
+            self.model = None
+            
+        return (output, )
 
 
 NODE_CLASS_MAPPINGS = {
