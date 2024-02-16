61cb56d21c8715a8a8b03aa6d13c95364b47d798
Ziad Abdeltawab <ziad.tawab2000@gmail.com>
2024-02-16 14:41:55 +0200

    Update openpose.py

--- node_wrappers/openpose.py
+++ node_wrappers/openpose.py
@@ -8,30 +8,36 @@ class OpenPose_Preprocessor:
         return create_node_input_types(
             detect_hand = (["enable", "disable"], {"default": "enable"}),
             detect_body = (["enable", "disable"], {"default": "enable"}),
-            detect_face = (["enable", "disable"], {"default": "enable"})
+            detect_face = (["enable", "disable"], {"default": "enable"}),
+            cache_model = (["enable", "disable"], {"default": "enable"}),
         )
         
     RETURN_TYPES = ("IMAGE", "POSE_KEYPOINT")
     FUNCTION = "estimate_pose"
 
     CATEGORY = "ControlNet Preprocessors/Faces and Poses Estimators"
-
-    def estimate_pose(self, image, detect_hand, detect_body, detect_face, resolution=512, **kwargs):
+    model = None
+    def estimate_pose(self, image, detect_hand, detect_body, detect_face, cache_model, resolution=512, **kwargs):
         from controlnet_aux.open_pose import OpenposeDetector
 
         detect_hand = detect_hand == "enable"
         detect_body = detect_body == "enable"
         detect_face = detect_face == "enable"
-
-        model = OpenposeDetector.from_pretrained().to(model_management.get_torch_device())        
+        cache_model = cache_model == "enable"
+        
+        if self.model is None:     
+            self.model = OpenposeDetector.from_pretrained().to(model_management.get_torch_device())        
         self.openpose_dicts = []
         def func(image, **kwargs):
-            pose_img, openpose_dict = model(image, **kwargs)
+            pose_img, openpose_dict = self.model(image, **kwargs)
             self.openpose_dicts.append(openpose_dict)
             return pose_img
         
         out = common_annotator_call(func, image, include_hand=detect_hand, include_face=detect_face, include_body=detect_body, image_and_json=True, resolution=resolution)
-        del model
+        
+        if not cache_model:
+            self.model = None
+            
         return {
             'ui': { "openpose_json": [json.dumps(self.openpose_dicts, indent=4)] },
             "result": (out, self.openpose_dicts)
